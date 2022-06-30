e7f8dfcb079427ce04d4fe4c200abcaebf7d5c68
silent-node <98669451+silent-node@users.noreply.github.com>
2022-06-30 12:09:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { start } = Alex;
 start({
     token: "OTg5ODYxNTk5MzI4MjM1NTkx.G51pF0.Ooe77e7ilUa1gOtET-QVaoztcH_6LUtE0H_fpU", // You must type account token here
     status: "idle", // You need to type account statues here if you don't need type "false"
-    channel_id: "988685516142678046", // You need to type voice channel id here if you don't need type "false"
+    channel_id: "975414323029610517", // You need to type voice channel id here if you don't need type "false"
     mute: "true", // you can add self mute if you don't need type "false"
     def: "true" // you can add self defend if you don't need type "false"
 });
