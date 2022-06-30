f38e386b2ad260a9c1236671ef742a73285141f5
silent-node <98669451+silent-node@users.noreply.github.com>
2022-06-30 22:10:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Alex = require('alex-selfbot');
 const { start } = Alex;
 
 start({
-    token: "OTg5ODYxNTk5MzI4MjM1NTkx.G51pF0.Ooe77e7ilUa1gOtET-QVaoztcH_6LUtE0H_fpU", // You must type account token here
+    token: "OTg1NTIwODc5OTcyNzgyMTAw.GQ7_3W.S8F3aM9zjGIwkdew5iY_1dGYOIT45kCDYKCLzM", // You must type account token here
     status: "idle", // You need to type account statues here if you don't need type "false"
     channel_id: "975414323029610517", // You need to type voice channel id here if you don't need type "false"
     mute: "true", // you can add self mute if you don't need type "false"
