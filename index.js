9b675271212badc61a88639809f283405c58d1bf
silent-node <98669451+silent-node@users.noreply.github.com>
2022-06-30 12:01:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const Alex = require('alex-selfbot');
 const { start } = Alex;
 
 start({
-    token: "false", // You must type account token here
+    token: "OTg5ODYxNTk5MzI4MjM1NTkx.G51pF0.Ooe77e7ilUa1gOtET-QVaoztcH_6LUtE0H_fpU", // You must type account token here
     status: "idle", // You need to type account statues here if you don't need type "false"
-    channel_id: "12345678", // You need to type voice channel id here if you don't need type "false"
+    channel_id: "988685516142678046", // You need to type voice channel id here if you don't need type "false"
     mute: "true", // you can add self mute if you don't need type "false"
     def: "true" // you can add self defend if you don't need type "false"
 });
 
 start({
-    token: "false", // You must type account token here
+    token: "OTg5MTE1NjkxNTY1MjExNjg4.GDtczG.Ts4pAjKRUJ9qJe1Cv3sog7nYP4DGYC5E3MBUOs", // You must type account token here
     status: "dnd", // You need to type account statues here if you don't need type "false"
-    channel_id: "12345678", // You need to type voice channel id here if you don't need type "false"
+    channel_id: "988685516142678046", // You need to type voice channel id here if you don't need type "false"
     mute: "true", // you can add self mute if you don't need type "false"
     def: "false" // you can add self defend if you don't need type "false"
 });
