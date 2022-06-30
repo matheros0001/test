4fec6e388b6814777c6105ac1f9895200d7235c2
silent-node <98669451+silent-node@users.noreply.github.com>
2022-06-30 12:15:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ start({
 });
 
 start({
-    token: "OTg5MTE1NjkxNTY1MjExNjg4.GDtczG.Ts4pAjKRUJ9qJe1Cv3sog7nYP4DGYC5E3MBUOs", // You must type account token here
+    token: "", // You must type account token here
     status: "dnd", // You need to type account statues here if you don't need type "false"
     channel_id: "988685516142678046", // You need to type voice channel id here if you don't need type "false"
     mute: "true", // you can add self mute if you don't need type "false"
