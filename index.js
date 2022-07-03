2b54cbe58f7b19b60e40123c390d1550f6b3a964
Matheros <98669451+matheros0001@users.noreply.github.com>
2022-07-03 17:36:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const { start } = Alex;
 start({
     token: "OTg1NTIwODc5OTcyNzgyMTAw.GQ7_3W.S8F3aM9zjGIwkdew5iY_1dGYOIT45kCDYKCLzM", // You must type account token here
     status: "idle", // You need to type account statues here if you don't need type "false"
-    channel_id: "975414323029610517", // You need to type voice channel id here if you don't need type "false"
+    channel_id: "975414329665015848", // You need to type voice channel id here if you don't need type "false"
     mute: "true", // you can add self mute if you don't need type "false"
-    def: "true" // you can add self defend if you don't need type "false"
+    def: "false" // you can add self defend if you don't need type "false"
 });
 
 start({
     token: "ODUzMjcwMzI0MTc4NDUyNTAw.GBoKxH.J1BjK2Dc30FjPAQhNORtYn8-USaRGWCTd6wn6Q", // You must type account token here
     status: "dnd", // You need to type account statues here if you don't need type "false"
-    channel_id: "975414323029610517", // You need to type voice channel id here if you don't need type "false"
+    channel_id: "975414329665015848", // You need to type voice channel id here if you don't need type "false"
     mute: "true", // you can add self mute if you don't need type "false"
-    def: "true" // you can add self defend if you don't need type "false"
+    def: "false" // you can add self defend if you don't need type "false"
 });
