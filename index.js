31f47747644f90b39eecac422ae9b8f40b75485d
Matheros <98669451+matheros0001@users.noreply.github.com>
2022-07-05 09:33:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,18 @@ start({
     def: "true" // you can add self defend if you don't need type "false"
 });
 
-start({
+/*start({
     token: "ODUzMjcwMzI0MTc4NDUyNTAw.GBoKxH.J1BjK2Dc30FjPAQhNORtYn8-USaRGWCTd6wn6Q", // You must type account token here
     status: "dnd", // You need to type account statues here if you don't need type "false"
     channel_id: "975414329665015848", // You need to type voice channel id here if you don't need type "false"
+    mute: "true", // you can add self mute if you don't need type "false"
+    def: "true" // you can add self defend if you don't need type "false"
+});*/
+
+start({
+    token: "", // You must type account token here
+    status: "dnd", // You need to type account statues here if you don't need type "false"
+    channel_id: "993916993214369842", // You need to type voice channel id here if you don't need type "false"
     mute: "true", // you can add self mute if you don't need type "false"
     def: "true" // you can add self defend if you don't need type "false"
 });
