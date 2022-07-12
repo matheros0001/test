7b063931586f328e79443462db08ac836eafa966
Matheros <98669451+matheros0001@users.noreply.github.com>
2022-07-12 14:53:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ start({
     def: "true" // you can add self defend if you don't need type "false"
 });
 
-/*start({
+start({
     token: "", // You must type account token here
     status: "dnd", // You need to type account statues here if you don't need type "false"
     channel_id: voice, // You need to type voice channel id here if you don't need type "false"
     mute: "true", // you can add self mute if you don't need type "false"
     def: "true" // you can add self defend if you don't need type "false"
-});*/
+});
 
 start({
     token: "", // You must type account token here
